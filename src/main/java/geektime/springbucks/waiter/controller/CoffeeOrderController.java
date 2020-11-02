7080dd9b6e2872dcc3a346e514c44a233d40e3e0
shenchaosong <shenchaosong@yidaoit.cn>
2020-11-02 20:00:17 +0800

    wip-thymeleaf

--- src/main/java/geektime/springbucks/waiter/controller/CoffeeOrderController.java
+++ src/main/java/geektime/springbucks/waiter/controller/CoffeeOrderController.java
@@ -1,6 +1,7 @@
 package geektime.springbucks.waiter.controller;
 
 
+import geektime.springbucks.waiter.controller.request.NewCoffeeRequest;
 import geektime.springbucks.waiter.controller.request.NewOrderRequest;
 import geektime.springbucks.waiter.model.Coffee;
 import geektime.springbucks.waiter.model.CoffeeOrder;
@@ -10,10 +11,19 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.validation.Valid;
+import java.util.Iterator;
+import java.util.List;
+import java.util.function.Consumer;
 
 
-@RestController
+@Controller
 @RequestMapping("/order")
 @Slf4j
 public class CoffeeOrderController {
@@ -37,6 +47,33 @@ public class CoffeeOrderController {
         Coffee[] coffeeList = coffeeService.getCoffeeByName(newOrder.getItems()).toArray(new Coffee[]{});
         return coffeeOrderService.createOrder(newOrder.getCustomer(), coffeeList);
     }
+
+    @ModelAttribute
+    public List<Coffee> coffeeList() {
+        List<Coffee> coffees = coffeeService.getAllCoffee();
+        return coffeeService.getAllCoffee();
+    }
+
+    @GetMapping(path = "/")
+    public ModelAndView showCreateForm(){
+        return new ModelAndView("create-order-form");
+    }
+
+
+    @PostMapping(path = "/",consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+    public String CreateOrder(@Valid NewOrderRequest newOrderRequest,
+                              BindingResult bindingResult, ModelMap modelMap){
+        if(bindingResult.hasErrors()){
+            log.warn("Binding Result: {}", bindingResult);
+            modelMap.addAttribute("message",bindingResult.toString());
+            return "create-order-form";
+        }
+        log.info("Receive new Order {}", newOrderRequest);
+        Coffee[] coffees = coffeeService.getCoffeeByName(newOrderRequest.getItems()).toArray(new Coffee[] {});
+        CoffeeOrder order = coffeeOrderService.createOrder(newOrderRequest.getCustomer(),coffees);
+        return "redirect:/order/" + order.getId();
+    }
+
 
 
 }
