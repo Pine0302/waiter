13e2094473785ae0d6362f4df1eeab0a66342fc0
shenchaosong <shenchaosong@yidaoit.cn>
2020-11-03 09:56:57 +0800

    mvc handlerInterceptor

--- src/main/java/geektime/springbucks/waiter/WaiterApplication.java
+++ src/main/java/geektime/springbucks/waiter/WaiterApplication.java
@@ -1,6 +1,7 @@
 package geektime.springbucks.waiter;
 
 import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module;
+import geektime.springbucks.waiter.controller.PerformanceInterceptor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
@@ -9,17 +10,26 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.TimeZone;
 
 @SpringBootApplication
 @EnableCaching
 @EnableAspectJAutoProxy
 @EnableTransactionManagement
-public class WaiterApplication {
+public class WaiterApplication implements WebMvcConfigurer {
 
     public static void main(String[] args) {
         SpringApplication.run(WaiterApplication.class, args);
     }
 
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new PerformanceInterceptor()).
+                addPathPatterns("/coffee/**").addPathPatterns("/order/**");
+    }
     @Bean
     public Hibernate5Module hibernate5Module() {
         return new Hibernate5Module();
@@ -27,11 +37,10 @@ public class WaiterApplication {
 
 	@Bean
 	public Jackson2ObjectMapperBuilderCustomizer jacksonBuilderCustomizer() {
-/*        new Jackson2ObjectMapperBuilder(
-
-        ).indentOutput(true);*/
-
-		return builder -> builder.indentOutput(true);
+		return bulider -> {
+            bulider.indentOutput(true);
+            bulider.timeZone(TimeZone.getTimeZone("Asia/Shanghai"));
+        };
 	}
 
 }
